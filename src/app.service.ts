67f7a16e57ef14f1ffb977f1bf87a7375e9304ef
Aman Sinha <aksinha.101010@gmail.com>
2024-06-07 20:19:21 +0530

    impl service for reconciliation

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,99 @@
 import { Injectable } from '@nestjs/common';
+import { ReconcileDto } from './dto/reconcile.dto';
+import { Contact } from './entities/contact.entity';
+import { QueryFailedError, Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @InjectRepository(Contact)
+    private contactRepository: Repository<Contact>,
+  ) {}
+
+  async reconcile(data: ReconcileDto): Promise<Contact> {
+    let finalContact: Contact;
+
+    try {
+      const contact = await this.contactRepository.findOneBy([
+        { email: data.email },
+        { phoneNumber: data.phoneNumber },
+      ]);
+
+      if (!contact) {
+        // If contact not present, create a primary contact
+        await this.contactRepository.insert({
+          phoneNumber: data.phoneNumber,
+          email: data.email,
+          linkPrecedence: 'primary',
+        });
+      } else if (contact.linkPrecedence == 'primary') {
+        if (
+          (data.email && data.email != contact.email) ||
+          (data.phoneNumber && data.phoneNumber != contact.phoneNumber)
+        ) {
+          // Create new secondary contact
+          await this.contactRepository
+            .insert({
+              email: data.email,
+              phoneNumber: data.phoneNumber,
+              linkPrecedence: 'secondary',
+              primaryContactId: contact.id,
+            })
+            .catch((e) => {
+              if (
+                !(e instanceof QueryFailedError) ||
+                e.driverError.code != '23505'
+              ) {
+                throw e;
+              }
+            });
+        }
+      } else {
+        if (
+          (data.email && data.email != contact.email) ||
+          (data.phoneNumber && data.phoneNumber != contact.phoneNumber)
+        ) {
+          // Create new secondary contact
+          await this.contactRepository
+            .insert({
+              email: data.email,
+              phoneNumber: data.phoneNumber,
+              linkPrecedence: 'secondary',
+              primaryContactId: contact.primaryContactId,
+            })
+            .catch((e) => {
+              if (
+                !(e instanceof QueryFailedError) ||
+                e.driverError.code != '23505'
+              ) {
+                throw e;
+              }
+            });
+        }
+      }
+
+      const where = this.contactRepository.create();
+      if (contact) {
+        where.id =
+          contact.linkPrecedence == 'primary'
+            ? contact.id
+            : contact.primaryContactId;
+      } else {
+        where.email = data.email;
+        where.phoneNumber = data.phoneNumber;
+      }
+
+      finalContact = await this.contactRepository.findOne({
+        where: where,
+        relations: {
+          secondaryContacts: true,
+        },
+      });
+    } catch (e) {
+      console.log({ e });
+      throw new Error('transaction failed');
+    }
+    return finalContact;
   }
 }
