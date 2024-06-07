0872308b3fed2cd714c4fbcc655b6c5f6dbc5963
Aman Sinha <aksinha.101010@gmail.com>
2024-06-07 20:17:27 +0530

    setup db connection and repository injection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Contact } from './entities/contact.entity';
+import config from './config/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [config],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService<Config>) => {
+        const dbConfig = configService.get<DbConfig>('database');
+        return {
+          type: 'postgres',
+          host: dbConfig.host,
+          port: dbConfig.port,
+          username: dbConfig.user,
+          password: dbConfig.password,
+          database: dbConfig.name,
+          entities: [Contact],
+          synchronize: true,
+        };
+      },
+      inject: [ConfigService],
+    }),
+    TypeOrmModule.forFeature([Contact]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
