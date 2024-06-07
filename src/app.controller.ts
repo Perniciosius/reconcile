a939cbfde4e6a7d083336012b51cfaf642de7c50
Aman Sinha <aksinha.101010@gmail.com>
2024-06-07 20:19:58 +0530

    add controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,45 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { ReconcileDto } from './dto/reconcile.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHealth(): string {
+    return 'ok';
+  }
+
+  @Post('/identify')
+  async reconcileContact(@Body() body: ReconcileDto): Promise<any> {
+    if (!body.email && !body.phoneNumber) {
+      throw new HttpException(
+        'Either email or phone number is required',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    const data = await this.appService.reconcile(body);
+    let phoneNumbers = [data.phoneNumber];
+    phoneNumbers = phoneNumbers.concat(
+      data.secondaryContacts?.map((v) => v.phoneNumber),
+    );
+    let emails = [data.email];
+    emails = emails.concat(data.secondaryContacts?.map((v) => v.email));
+    return {
+      contact: {
+        primaryContactId: data.id,
+        emails: [...new Set(emails.filter((v) => v))],
+        phoneNumbers: [...new Set(phoneNumbers.filter((v) => v))],
+        secondaryContactIds: data.secondaryContacts?.map((v) => v.id),
+      },
+    };
   }
 }
